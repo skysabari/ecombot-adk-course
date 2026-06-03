1ae3ad3e59eb33bb6c9bd104ffe164e677717bf3
skysabari <sabariraj.ganeshbabu@skytv.it>
2026-06-03 18:41:54 +0530

    feat(day-1): uploading day 1 code

--- src/agents/support_agent.py
+++ src/agents/support_agent.py
@@ -10,7 +10,6 @@ from config.settings import APP_NAME, MODEL
 litellm.suppress_debug_info = True
 load_dotenv()
 
-# _INSTRUCTION = (Path(__file__).parent / "instruction.txt").read_text().strip()
 
 root_agent = LlmAgent(
     name=APP_NAME,
